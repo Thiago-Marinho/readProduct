1fe81ef9f76718fd200db299901903f71fb4e793
Thiago-Marinho <peres.marinho@gmail.com>
2021-08-05 16:30:57 -0400

    Adição de classes, atributos e metodos

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class ImportedProduct extends Product{
+
+	private Double customFee;
+	
+	public ImportedProduct() {
+		super();
+	}
+
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;
+	}
+
+	public Double getCustomFee() {
+		return customFee;
+	}
+
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	@Override
+	public String priceTag() {		
+		return super.getName() +", $"+ priceTotal() +"(Customs fee: $"+ String.format("%.2f", customFee) +")";
+	}
+	
+	public Double priceTotal() {
+		
+		return super.getPrice() + customFee;
+	}
+}
