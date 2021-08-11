d130b0433a8bbb0114f6d060beac3d0913ae9f27
Thiago-Marinho <peres.marinho@gmail.com>
2021-08-11 15:41:55 -0400

    Programa implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,58 @@
 package application;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-
+		Scanner sc = new Scanner(System.in);
+		
+		List<Product> list = new ArrayList<>();
+		System.out.print("Enter the number of products:  ");
+		int n = sc.nextInt();		
+		
+		for(int i=1; i <=n; i++) {
+			System.out.println("Product #"+ i +" data:");
+			System.out.print("Common, used or imported (c/u/i)?");
+			char option = sc.next().charAt(0);
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			Double price = sc.nextDouble();
+			
+			if(option == 'c') {
+				list.add(new Product(name, price));
+			
+			}else if(option == 'i') {
+				
+				System.out.print("Customs fee: ");
+				Double customFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customFee));
+				
+			}else if(option == 'u') {
+				
+				System.out.print("Manufature date (DD/MM/YYYY): ");
+				String date = sc.next();
+				list.add(new UsedProduct(name, price, date));
+			}
+			
+		}
+		System.out.println("\nPRICE TAGS: ");
+		for (Product pdt : list) {
+			System.out.println(pdt.priceTag());
+		}
+		
+		sc.close();	
 	}
 
 }
--- src/entities/UsedProduct.java
+++ src/entities/UsedProduct.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class UsedProduct {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class UsedProduct extends Product{
+	
+	private Date manufatureDate;
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public UsedProduct() {
+		super();
+	}
+
+	public UsedProduct(String name, Double price, String manufatureDate) throws ParseException {
+		super(name, price);
+		Date m = sdf.parse(manufatureDate);
+		this.manufatureDate = m;
+	}
+
+	public Date getManufatureDate() {
+		return manufatureDate;
+	}
+
+	public void setManufatureDate(Date manufatureDate) {
+		this.manufatureDate = manufatureDate;
+	}
+	
+	@Override
+	public String priceTag() {
+		return super.getName() +"(used), $"+ super.getPrice() +"(manufature date: "+ sdf.format(manufatureDate)+ ")";
+	}
 }
